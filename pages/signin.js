9a32a4c06f186aef98ea85f3c527dabbcdf7821e
dami-josif <oluwadamilare985@gmail.com>
2023-08-14 19:19:10 +0100

    test

--- pages/signin.js
+++ pages/signin.js
@@ -1,4 +1,5 @@
 import { useState,useEffect,useContext } from "react";
+import Image from "next/image";
 // import { AppContext } from "@/settings/context/appContext";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -12,8 +13,8 @@ import {FiTwitter} from 'react-icons/fi';
 import MainNav from "@/components/MainNav";
 import Footer from "@/components/Footer";
 import {signIn} from 'next-auth/react';
-import { getServerSession } from "next-auth/next";
-import { NextAuthOptions } from "./api/auth/[...nextauth]";
+// import { getServerSession } from "next-auth/next";
+// import { NextAuthOptions } from "./api/auth/[...nextauth]";
 // import { useSession } from "next-auth/react";
 
 //create a validation schema (validation rules)
@@ -52,7 +53,7 @@ export default function Signin () {
     });
 
     return (
-        <div className="bg-gradient-to-r from-gray-300 to-orange-200">
+        <div className="">
         <Head>
             <title>Sign in | Real Fast</title>
             <meta name="description" content="Sign in to Real Fast and start applying for jobs" />
@@ -61,67 +62,70 @@ export default function Signin () {
         </Head>
         <MainNav/>
         <main className={styles.container}>
-            <div className={styles.wrapper}>
-                <h2 className={styles.title}>SIGN IN TO YOUR ONLYSHOES ACCOUNT</h2>
+            <h2 className={styles.title}>SIGN IN TO YOUR ONLYSHOES ACCOUNT</h2>
+            <div className="lg:flex gap-3">
+                <Image width={800} height={1700} src="/images/pageimage.jpg" alt=""/>
+                <div className={styles.wrapper}>
 
-                <form autoComplete="off" onSubmit={handleSubmit}>
-                    <div className="flex justify-end">
-                        <p className="text-md text-gray-700 font-bold flex flex-row gap-3 mb-3" onClick={() => authChoice ? setAuthChoice(false) : setAuthChoice(true)}>
-                            <span>Sign in with {authChoice ? 'credentials' : 'email'}</span>
-                            <AiOutlineUndo className="text-gray-800 font-bold text-2xl"/>
-                        </p>
-                    </div>
-                    <div className={styles.inputBlockMain}>
-                        <label className={styles.label}>Email address</label>
-                        <input 
-                        id="email"
-                        type="email" 
-                        placeholder="valid email address"
-                        className={styles.inputField}
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}/>
-                        {
-                            errors.email && touched.email 
-                            ? <p className={styles.formError} style={{color:'red'}}>{errors.email}</p>
-                            : null
-                        }
-                    </div>
+                    <form autoComplete="off" onSubmit={handleSubmit}>
+                        <div className="flex justify-end">
+                            <p className="text-md text-gray-700 font-bold flex flex-row gap-3 mb-3" onClick={() => authChoice ? setAuthChoice(false) : setAuthChoice(true)}>
+                                <span>Sign in with {authChoice ? 'credentials' : 'email'}</span>
+                                <AiOutlineUndo className="text-gray-800 font-bold text-2xl"/>
+                            </p>
+                        </div>
+                        <div className={styles.inputBlockMain}>
+                            <label className={styles.label}>Email address</label>
+                            <input 
+                            id="email"
+                            type="email" 
+                            placeholder="valid email address"
+                            className={styles.inputField}
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}/>
+                            {
+                                errors.email && touched.email 
+                                ? <p className={styles.formError} style={{color:'red'}}>{errors.email}</p>
+                                : null
+                            }
+                        </div>
 
-                    <div className={styles.inputBlockMain} style={{display:authChoice ? 'none' : 'block'}}>
-                        <label className={styles.label}>Password</label>
-                        <input 
-                        id="password"
-                        type="password" 
-                        className={styles.inputField}
-                        value={values.password}
-                        onChange={handleChange}
-                        onBlur={handleBlur}/>
-                        {
-                            errors.password && touched.password 
-                            ? <p className={styles.formError} style={{color:'red'}}>{errors.password}</p>
-                            : null
-                        }
-                    </div>
+                        <div className={styles.inputBlockMain} style={{display:authChoice ? 'none' : 'block'}}>
+                            <label className={styles.label}>Password</label>
+                            <input 
+                            id="password"
+                            type="password" 
+                            className={styles.inputField}
+                            value={values.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}/>
+                            {
+                                errors.password && touched.password 
+                                ? <p className={styles.formError} style={{color:'red'}}>{errors.password}</p>
+                                : null
+                            }
+                        </div>
 
-                    <button type="submit" className={styles.submitBtn}
-                    onClick={()=>{
-                        if (authChoice) {
-                            signIn('email',{
-                                email:values.email,
-                                redirect:false
-                            })
-                        }else{
-                            signIn('credentials',{email:values.email,password:values.password,redirect:false})
-                        }
-                    }}>Sign In</button>
-                </form>
-                <p className="text-lg text-center my-2 font-bold">OR, Sign in with</p>
-                <div className={styles.or}>
-                    <button className={styles.signinBtn } onClick={()=> signIn('google')}> <p className="px-3">Google</p> <FcGoogle className="mt-2"/></button>
-                    <button className={styles.signinBtn } onClick={()=> signIn('github')}><p className="px-3">GitHub</p><AiFillGithub className="mt-2"/></button>
-                    <button className={styles.signinBtn } onClick={()=> signIn('twitter')}><p className="px-3">Twitter</p><FiTwitter className="mt-2"/></button>
-                </div>
+                        <button type="submit" className={styles.submitBtn}
+                        onClick={()=>{
+                            if (authChoice) {
+                                signIn('email',{
+                                    email:values.email,
+                                    redirect:false
+                                })
+                            }else{
+                                signIn('credentials',{email:values.email,password:values.password,redirect:false})
+                            }
+                        }}>Sign In</button>
+                    </form>
+                    <p className="text-lg text-center my-2 font-bold">OR, Sign in with</p>
+                    <div className={styles.or}>
+                        <button className={styles.signinBtn } onClick={()=> signIn('google')}> <p className="px-3">Google</p> <FcGoogle className="mt-2"/></button>
+                        <button className={styles.signinBtn } onClick={()=> signIn('github')}><p className="px-3">GitHub</p><AiFillGithub className="mt-2"/></button>
+                        <button className={styles.signinBtn } onClick={()=> signIn('twitter')}><p className="px-3">Twitter</p><FiTwitter className="mt-2"/></button>
+                    </div>
+            </div>
             </div>
         </main>
         <Footer/>
@@ -129,46 +133,46 @@ export default function Signin () {
     )
 }
 
-export async function getServerSideProps (context){
-    const session = await getServerSession(context.req,context.res,NextAuthOptions);
+// export async function getServerSideProps (context){
+//     const session = await getServerSession(context.req,context.res,NextAuthOptions);
 
-    //if there is an active session, redirect to talent dashboard
-    if(session){
-       if (session.user.accountType == 'admin') {
-            return {
-                redirect:{
-                    destination:'/',
-                    permanent:false,
-                }
-            }
-       }else if(session.user.accountType == 'user') {
-            return {
-                redirect:{
-                    destination:'/',
-                    permanent:false,
-                }
-            }
-       }
-    }
+//     //if there is an active session, redirect to talent dashboard
+//     if(session){
+//        if (session.user.accountType == 'admin') {
+//             return {
+//                 redirect:{
+//                     destination:'/',
+//                     permanent:false,
+//                 }
+//             }
+//        }else if(session.user.accountType == 'user') {
+//             return {
+//                 redirect:{
+//                     destination:'/',
+//                     permanent:false,
+//                 }
+//             }
+//        }
+//     }
 
-    return{
-        props:{
-            session:JSON.parse(JSON.stringify(session))
-        }
-    }
-}
+//     return{
+//         props:{
+//             session:JSON.parse(JSON.stringify(session))
+//         }
+//     }
+// }
 
 const styles = {
-    container:'w-full flex flex-col justify-center items-center px-16 lg:mt-32 lg:mb-40 mt-12 mb-40',
-    wrapper:'w-full md:w-[720px] flex flex-col gap-16',
+    container:'w-full flex flex-col justify-center items-center px-16 lg:mt-16 lg:mb-40 mt-12 mb-40',
+    wrapper:'w-100 md:w-[720px] flex flex-col gap-16',
     title:'font-heading text-xl font-bold text-gray-900 text-center',
     inputBlockRow:'w-full flex flex-col md:flex-row md:gap-3 md:mb-4',
     inputBlock:'w-full mb-4',
     inputBlockMain:'w-full mb-4',
     label:'text-gray-700 mb-2',
     inputField:'w-full block border border-gray-500 py-3 px-4 rounded',
-    submitBtn:'w-full bg-orange-800 py-5 px-4 rounded-full text-lg font-bold text-white',
+    submitBtn:'w-full bg-orange-800 py-5 px-4 rounded text-lg font-bold text-white',
     formError:'text-xs',
-    or:'w-full text-2xl flex flex-col lg:flex-row gap-3 justify-center mb-5',
+    or:'w-50 text-2xl flex flex-col gap-3 justify-center mb-5',
     signinBtn:'lg:px-20 py-2 flex justify-center rounded  border border-gray-900'
 }
--- pages/signup.js
+++ pages/signup.js
@@ -53,74 +53,76 @@ export default function Signup () {
 
     return (
         <>
-            <Head>
-                <title>Create Account | OnlyShoes</title>
-                <meta name="description" content="Create an account on Real Fast and start applying for jobs" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/realfast_logo.png" />
-            </Head>
-            <MainNav/>
-            <main className={styles.container} >
-                <div className={styles.wrapper}>
-                    <h2 className={styles.title}>Create an OnlyShoes Account</h2>
+            <div className="bg-gradient-to-r from-gray-300 to-orange-200">
+                <Head>
+                    <title>Create Account | OnlyShoes</title>
+                    <meta name="description" content="Create an account on Real Fast and start applying for jobs" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="icon" href="/realfast_logo.png" />
+                </Head>
+                <MainNav/>
+                <main className={styles.container} >
+                    <div className={styles.wrapper}>
+                        <h2 className={styles.title}>CREATE YOUR ONLYSHOES ACCOUNT</h2>
 
-                    <form autoComplete="off" onSubmit={handleSubmit}>
-                        <div className={styles.inputBlockMain}>
-                            <label className={styles.label}>Email address</label>
-                            <input 
-                            id="email"
-                            type="email" 
-                            placeholder="valid email address"
-                            className={styles.inputField}
-                            value={values.email}
-                            onChange={handleChange}
-                            onBlur={handleBlur}/>
-                            {
-                                errors.email && touched.email 
-                                ? <p className={styles.formError} style={{color:'red'}}>{errors.email}</p>
-                                : null
-                            }
-                        </div>
-
-                        <div className={styles.inputBlockRow}>
-                            <div className={styles.inputBlock}>
-                                <label className={styles.label}>Create password</label>
+                        <form autoComplete="off" onSubmit={handleSubmit}>
+                            <div className={styles.inputBlockMain}>
+                                <label className={styles.label}>Email address</label>
                                 <input 
-                                id="password"
-                                type="password" 
+                                id="email"
+                                type="email" 
+                                placeholder="valid email address"
                                 className={styles.inputField}
-                                value={values.password}
+                                value={values.email}
                                 onChange={handleChange}
                                 onBlur={handleBlur}/>
                                 {
-                                    errors.password && touched.password 
-                                    ? <p className={styles.formError} style={{color:'red'}}>{errors.password}</p>
+                                    errors.email && touched.email 
+                                    ? <p className={styles.formError} style={{color:'red'}}>{errors.email}</p>
                                     : null
                                 }
                             </div>
-                            <div className={styles.inputBlock}>
-                                <label className={styles.label}>Confirm password</label>
-                                <input 
-                                id="passwordConfirmation"
-                                type="password" 
-                                className={styles.inputField}
-                                value={values.passwordConfirmation}
-                                onChange={handleChange}
-                                onBlur={handleBlur}/>
-                            </div>
-                        </div>
 
-                        <button type="submit" className={styles.submitBtn}>Create Account</button>
-                    </form>
-                </div>
-            </main>
-            <Footer/>
+                            <div className={styles.inputBlockRow}>
+                                <div className={styles.inputBlock}>
+                                    <label className={styles.label}>Create password</label>
+                                    <input 
+                                    id="password"
+                                    type="password" 
+                                    className={styles.inputField}
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}/>
+                                    {
+                                        errors.password && touched.password 
+                                        ? <p className={styles.formError} style={{color:'red'}}>{errors.password}</p>
+                                        : null
+                                    }
+                                </div>
+                                <div className={styles.inputBlock}>
+                                    <label className={styles.label}>Confirm password</label>
+                                    <input 
+                                    id="passwordConfirmation"
+                                    type="password" 
+                                    className={styles.inputField}
+                                    value={values.passwordConfirmation}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}/>
+                                </div>
+                            </div>
+
+                            <button type="submit" className={styles.submitBtn}>Create Account</button>
+                        </form>
+                    </div>
+                </main>
+                <Footer/>
+            </div>
         </>
     )
 }
 
 const styles = {
-    container:'w-full flex flex-col justify-center items-center px-16 lg:mt-32 lg:mb-40 mt-12 mb-40',
+    container:'w-full flex flex-col justify-center items-center px-16 lg:mt-32 lg:mb-60 mt-12 mb-40',
     wrapper:'w-full md:w-[720px] flex flex-col gap-16',
     title:'font-heading text-xl font-bold text-gray-900 text-center',
     inputBlockRow:'w-full flex flex-col md:flex-row md:gap-3 md:mb-4',
@@ -128,6 +130,6 @@ const styles = {
     inputBlockMain:'w-full mb-4',
     label:'text-gray-700 mb-2',
     inputField:'w-full block border border-gray-500 py-3 px-4 rounded',
-    submitBtn:'w-full bg-orange-800 font-bold py-5 px-4 rounded-full text-lg text-white',
+    submitBtn:'w-full bg-orange-800 font-bold py-5 px-4 rounded text-lg text-white',
     formError:'text-xs'
 }
